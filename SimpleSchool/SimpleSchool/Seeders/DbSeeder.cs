60bc6427a051fafeb0ef99d5e73ba4d2d223af53
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-08 18:30:23 +0200

    Seeders aangemaakt

--- SimpleSchool/SimpleSchool/Seeders/DbSeeder.cs
+++ SimpleSchool/SimpleSchool/Seeders/DbSeeder.cs
@@ -1,7 +1,13 @@
-﻿namespace SimpleSchool.Seeders
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSchool.Seeders
 {
     public class DbSeeder : ISeeder
     {
-
+        public void Seed(ModelBuilder modelBuilder)
+        {
+            List<ISeeder> seeders = new() { new LeerkreachtSeeder(), new LeerlingSeeder(), new VakSeeder(), new StudentenkaartSeeder(), new OpleidingSeeder() };
+            seeders.ForEach(seeder => seeder.Seed(modelBuilder));
+        }
     }
 }
--- SimpleSchool/SimpleSchool/Seeders/LeerlingSeeder.cs
+++ SimpleSchool/SimpleSchool/Seeders/LeerlingSeeder.cs
@@ -1,6 +1,15 @@
-﻿namespace SimpleSchool.Seeders
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleSchool.Fakers;
+using SimpleschoolApp.Models;
+
+namespace SimpleSchool.Seeders
 {
-    public class LeerlingSeeder
+    public class LeerlingSeeder : ISeeder
     {
+        public void Seed(ModelBuilder modelBuilder)
+        {
+            List<Leerling> leerlingen = LeerlingFaker.Faker.Generate(10);
+            modelBuilder.Entity<Leerling>().HasData(leerlingen);
+        }
     }
 }
