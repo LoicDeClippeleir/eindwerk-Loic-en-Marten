fea3486f4b5310502fa508193ccee0993edeb79e
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-08 18:45:36 +0200

    Studenten en vak Faker aangemaakt

--- SimpleSchool/SimpleSchool/Fakers/StudentenKaartFaker.cs
+++ SimpleSchool/SimpleSchool/Fakers/StudentenKaartFaker.cs
@@ -1,6 +1,14 @@
-﻿namespace SimpleSchool.Fakers
+﻿using Bogus;
+using SimpleschoolApp.Models;
+
+namespace SimpleSchool.Fakers
 {
     public class StudentenKaartFaker
     {
+        public static readonly Faker<StudentenKaart> Faker =  new Faker<StudentenKaart>().RuleFor(l => l.Id, f => f.IndexFaker + 1)
+                                                                                         .RuleFor(l => l.Naam, f => f.Name.FindName())
+                                                                                         .RuleFor(l => l.School, f => f.Company.CompanyName())
+                                                                                         ;
+
     }
 }
--- SimpleSchool/SimpleSchool/Fakers/VakFaker.cs
+++ SimpleSchool/SimpleSchool/Fakers/VakFaker.cs
@@ -1,6 +1,15 @@
-﻿namespace SimpleSchool.Fakers
+﻿using Bogus;
+using SimpleschoolApp.Models;
+
+namespace SimpleSchool.Fakers
 {
     public class VakFaker
     {
+        public static readonly Faker<Vak> Faker = new Faker<Vak>().RuleFor(l => l.Id, f => f.IndexFaker + 1)
+                                                                  .RuleFor(l => l.Naam, f => f.Name.FindName())
+                                                                  .RuleFor(l => l.AantalStudiePunten, f => f.Random.Number(10, 50))
+                                                                  .RuleFor(l => l.Vaktype, f => f.Random.Enum<VakType>());
+                        
+                                                                  
     }
 }
