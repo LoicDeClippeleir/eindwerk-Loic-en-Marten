69b98e063c4e989fff89c34d2b4855bb8fde3202
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-10 13:10:45 +0200

    tzelfde maar correct

--- SimpleSchool/SimpleSchool/Fakers/OpleidingFaker.cs
+++ SimpleSchool/SimpleSchool/Fakers/OpleidingFaker.cs
@@ -5,7 +5,7 @@ namespace SimpleSchool.Fakers
 {
     public class OpleidingFaker
     {
-        public static readonly Faker<Opleiding>("nl") Faker = new Faker<Opleiding>()
+        public static readonly Faker<Opleiding> Faker = new Faker<Opleiding>("nl")
            .RuleFor(l => l.Id, f => f.IndexFaker + 1)
            .RuleFor(l => l.Naam, f => f.Lorem.Sentence(20))
            .RuleFor(l => l.Duur, f => f.Random.Number(3,4))
