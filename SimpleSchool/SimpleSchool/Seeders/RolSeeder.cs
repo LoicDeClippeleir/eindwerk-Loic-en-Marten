80fe5cbbf7cf3ad7cd390965144ba24a7003a7f1
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-10 13:14:32 +0200

    Identitty role sedder

---
+++ SimpleSchool/SimpleSchool/Seeders/RolSeeder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus.DataSets;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleSchool.Seeders
+{
+    public class RolSeeder : ISeeder
+    {
+        public void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>().HasData( new IdentityRole { Name = "Leerling", NormalizedName = "LEERLING" }, 
+                                                         new IdentityRole { Name = "Leerkracht", NormalizedName = "LEERKRACHT"},
+                                                         new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" });
+        }
+    }
+}
