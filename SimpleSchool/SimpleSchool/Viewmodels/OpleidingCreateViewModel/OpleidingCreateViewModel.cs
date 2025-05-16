3975c769ceada3af6af3085d3188a2a2f5ef705e
Loïc De Clippeleir <loic.declippeleir@student.romerocollege.be>
2025-05-16 15:14:47 +0200

    opleidingViewModel toegevoegd

---
+++ SimpleSchool/SimpleSchool/Viewmodels/OpleidingCreateViewModel/OpleidingCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Simpleschool.Models;
+
+namespace SimpleSchool.Viewmodels.OpleidingCreateViewModel
+{
+    public class OpleidingCreateViewModel
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public int Duur { get; set; }
+        public int BeschikbarePlaatsen { get; set; }
+        public string Beschrijving { get; set; }
+        //public List<Leerling> Leerlingen { get; set; }
+        public List<Vak> Vakken { get; set; }
+    }
+}
