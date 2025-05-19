ae71cd25297fad5422f50ec45dc6fb34feed0476
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-19 10:22:07 +0200

    opleidingCreateViewModel verbeterd

--- SimpleSchool/SimpleSchool/Viewmodels/OpleidingCreateViewModel/OpleidingCreateViewModel.cs
+++ SimpleSchool/SimpleSchool/Viewmodels/OpleidingCreateViewModel/OpleidingCreateViewModel.cs
@@ -1,15 +1,24 @@
-﻿using Simpleschool.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace SimpleSchool.Viewmodels.OpleidingCreateViewModel
+namespace SimpleSchool.Viewmodels.Opleiding
 {
     public class OpleidingCreateViewModel
     {
-        public int Id { get; set; }
-        public string Naam { get; set; }
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        [StringLength(100, ErrorMessage = "Naam mag maximaal 100 tekens zijn.")]
+        public string Naam { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Duur is verplicht.")]
+        [Range(1, 5, ErrorMessage = "Duur moet tussen 1 en 5 jaar zijn.")]
         public int Duur { get; set; }
+
+        [Required(ErrorMessage = "Beschikbare plaatsen zijn verplicht.")]
+        [Range(1, 1000, ErrorMessage = "Beschikbare plaatsen moeten tussen 1 en 1000 zijn.")]
         public int BeschikbarePlaatsen { get; set; }
-        public string Beschrijving { get; set; }
-        //public List<Leerling> Leerlingen { get; set; }
-        public List<Vak> Vakken { get; set; }
+
+        [Required(ErrorMessage = "Beschrijving is verplicht.")]
+        [StringLength(1000, ErrorMessage = "Beschrijving mag maximaal 1000 tekens zijn.")]
+        public string Beschrijving { get; set; } = string.Empty;
     }
 }
+
