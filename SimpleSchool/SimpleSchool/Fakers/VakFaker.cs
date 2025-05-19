bf9dd2ea6a411fbc9ee9bee293635628e6858bc4
Loïc De Clippeleir <loic.declippeleir@student.romerocollege.be>
2025-05-19 10:44:33 +0200

    Faker aangepast

--- SimpleSchool/SimpleSchool/Fakers/VakFaker.cs
+++ SimpleSchool/SimpleSchool/Fakers/VakFaker.cs
@@ -6,7 +6,7 @@ namespace SimpleSchool.Fakers
 {
     public class VakFaker
     {
-        public static readonly Faker<Vak> Faker = new Faker<Vak>("nl").RuleFor(l => l.Id, f => f.IndexFaker + 1)
+        public Faker<Vak> Faker = new Faker<Vak>("nl").RuleFor(l => l.Id, f => f.IndexFaker + 1)
                                                                   .RuleFor(l => l.Naam, f => f.Name.FindName())
                                                                   .RuleFor(l => l.AantalStudiePunten, f => f.Random.Number(10, 50))
                                                                   .RuleFor(l => l.Vaktype, f => f.Random.Enum<VakType>())
