4385320866ea4880d6f7e7af59b04b798b90fd16
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-19 10:30:30 +0200

    Vak en studenten viewmodel toegevoegd

---
+++ SimpleSchool/SimpleSchool/Viewmodels/Studentenkaart/StudentenkaartCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSchool.Viewmodels.Studentenkaart
+{
+    public class StudentenkaartCreateViewModel
+    {
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        public string Naam { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Klas is verplicht.")]
+        public string Klas { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "School is verplicht.")]
+        public string School { get; set; } = string.Empty;
+    }
+}
+
---
+++ SimpleSchool/SimpleSchool/Viewmodels/Vak/VakCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Simpleschool.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSchool.Viewmodels.Vak
+{
+    public class VakCreateViewModel
+    {
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        [StringLength(100, ErrorMessage = "Naam mag maximaal 100 tekens zijn.")]
+        public string Naam { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Taal is verplicht.")]
+        [StringLength(50, ErrorMessage = "Taal mag maximaal 50 tekens zijn.")]
+        public string Taal { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Aantal studiepunten is verplicht.")]
+        [Range(1, 100, ErrorMessage = "Aantal studiepunten moet tussen 1 en 100 zijn.")]
+        public int AantalStudiePunten { get; set; }
+
+        [Required(ErrorMessage = "Vaktype is verplicht.")]
+        public VakType Vaktype { get; set; }
+
+        [Required(ErrorMessage = "Leerkracht is verplicht.")]
+        public int LeerkrachtId { get; set; }
+    }
+}
