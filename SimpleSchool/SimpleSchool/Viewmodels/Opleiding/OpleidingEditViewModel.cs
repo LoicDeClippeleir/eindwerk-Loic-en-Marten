aac5b276743956820ce6a180e34e1ab569038745
Marten De Clippeleir <marten.declippeleir@student.romerocollege.be>
2025-05-26 10:36:28 +0200

    LeerkrachtEditViewMOdel

---
+++ SimpleSchool/SimpleSchool/Viewmodels/Opleiding/OpleidingEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSchool.Viewmodels.Opleiding
+{
+    public class OpleidingEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        [StringLength(100, ErrorMessage = "Naam mag maximaal 100 tekens zijn.")]
+        public string Naam { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Duur is verplicht.")]
+        [Range(1, 10, ErrorMessage = "Duur moet tussen 1 en 10 jaar zijn.")]
+        public int Duur { get; set; }
+
+        [Required(ErrorMessage = "Beschikbare plaatsen zijn verplicht.")]
+        [Range(1, 1000, ErrorMessage = "Beschikbare plaatsen moeten tussen 1 en 1000 zijn.")]
+        public int BeschikbarePlaatsen { get; set; }
+
+        [Required(ErrorMessage = "Beschrijving is verplicht.")]
+        [StringLength(1000, ErrorMessage = "Beschrijving mag maximaal 1000 tekens zijn.")]
+        public string Beschrijving { get; set; } = string.Empty;
+    }
+}
+
---
+++ SimpleSchool/SimpleSchool/Viewmodels/Studentenkaart/StudentenkaartEditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSchool.Viewmodels.Studentenkaart
+{
+    public class StudentenkaartEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        [StringLength(100, ErrorMessage = "Naam mag maximaal 100 tekens zijn.")]
+        public string Naam { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Klas is verplicht.")]
+        [StringLength(50, ErrorMessage = "Klas mag maximaal 50 tekens zijn.")]
+        public string Klas { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "School is verplicht.")]
+        [StringLength(100, ErrorMessage = "School mag maximaal 100 tekens zijn.")]
+        public string School { get; set; } = string.Empty;
+    }
+}
+
